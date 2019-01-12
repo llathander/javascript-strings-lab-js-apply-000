aa839d3f9d6182c9d246ebdf46282a2b0b780c6c
Adrian Samaniego <llathander@gmail.com>
2019-01-12 19:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello" + specialGuest;
 
-var specialGuest = "Hello, Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = 'space';
 
